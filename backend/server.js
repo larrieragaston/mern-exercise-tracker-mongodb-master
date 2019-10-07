90cceebfef3b1c3a42d6290d699cd0b0c8bb11de
Gaston Larriera <larriera.gaston@gmail.com>
2019-10-07 09:07:05 -0300

    Purge

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-//const uri = process.env.LOCAL_URI;
+// const uri = process.env.ATLAS_URI;
+const uri = process.env.LOCAL_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
